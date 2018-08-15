f5510d3163e79e264d71853199ee769f2ccbc437
RoyzellW <royzellwalton@gmail.com>
2018-08-15 13:16:54 -0500

    Done

---
+++ Min.js
@@ -0,0 +1,26 @@
+//Print the first p tag to the console using the ID, class, and tag selectors.
+
+// var first1 = document.getElementById(".first");
+// var first2 = document.getElementsByClassName("#special");
+// var first3 = document.getElementsByTagName("p");
+//
+//
+// console.log(first3);
+// console.log(first2);
+// console.log(first1);
+
+// // Print the special class using both the query selector and query all selector.
+//  var special1 = document.querySelector(".special");
+//  var special2 = document.querySelectorAll("P.special");
+//
+//
+
+// Change the color of the h1 statement to blue.
+document.getElementsByTagName("h1").style.color = "blue";
+
+
+document.querySelector("h1").style.color = "blue";
+//
+// //Change the color of the last p tag to yellow.
+//
+// document.getElementById(last).style.color = "yellow";
